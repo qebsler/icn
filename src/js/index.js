29c37144f710f5886af2d75ee715286fb696279b
qebsler <dguillet6@gmail.com>
2018-11-17 21:39:18 +0100

    1.0.1

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,19 @@ const term = document.querySelector('section#right-side-term')
 
 li.forEach(el => {
   el.addEventListener('click', (e) => {
+    let text
     let p = document.createElement('p')
     if (e.target.dataset.content !== 'images') {
-      p.innerHTML = `<p class='${e.target.id}'>${e.target.dataset.content} </p><br>`
+      text = '<span class="blue">' + e.target.textContent + '</span><br>' + e.target.dataset.content
     } else {
-      p.innerHTML = `<p class='${e.target.id}'>images: <br><img src="assets/screen_1.png" alt="screen of the application"><img src="assets/screen_2.png" alt="screen of the application"><img src="assets/screen_3.png" alt="screen of the application"></p><br>`
+      text = '<span class="blue">' + e.target.textContent + '</span><br>' +
+      '<a href="assets/screen_1.png" target="_blank">screen_1.png</a><br>' +
+      '<a href="assets/screen_2.png" target="_blank">screen_2.png</a><br>' +
+      '<a href="assets/screen_3.png" target="_blank">screen_3.png</a><br>'
+      console.log(e.target.textContent)
     }
+    p.innerHTML = text
+    p.classList.add(e.target.id)
     term.appendChild(p)
   })
 })
